3e82d1aaa8e72b7c87d01356fb877d4eb069a77b
Hosub Lee <spyrr83@gmail.com>
2022-01-11 17:45:25 +0900

    SNORT: update build script
    - add snort3 community rules
    - update build scripts
    
    Signed-off-by: Hosub Lee <spyrr83@gmail.com>

---
+++ snort/files/rules/rules.lua
@@ -0,0 +1,2 @@
+include /snort/etc/rules/local.rules
+include /snort/etc/rules/community/snort3-community.rules
--- snort/files/run.sh
+++ snort/files/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ip link set dev ${NET_IF} promisc on
 ethtool -K ${NET_IF} gro off lro off
-/snort/bin/snort -D -i ${NET_IF} -c ${SNORT_CONF} -A ${SNORT_OUTMODE} ${SNORT_OPT}
+exec /snort/bin/snort -i ${NET_IF} -c ${SNORT_CONF} -A ${SNORT_OUTMODE} ${SNORT_OPT}
